afc4cb2da4b922b2602b6eaf3e29b2e029497c12
RogueNPC <jordanbtansay@gmail.com>
2020-11-30 12:19:10 -0800

    added all pages & working navbar

--- app.py
+++ app.py
@@ -14,5 +14,20 @@ def homepage():
     """The homepage"""
     return render_template('home.html')
 
+@app.route('/about')
+def aboutpage():
+    """About us"""
+    return render_template('about.html')
+
+@app.route('/menu')
+def menupage():
+    """Our menu"""
+    return render_template('menu.html')
+
+@app.route('/contact')
+def contactpage():
+    """How to contact"""
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
