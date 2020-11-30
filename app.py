16fb0a767190ce85652f2146ea92caf3713d86f3
RogueNPC <jordanbtansay@gmail.com>
2020-11-30 11:52:25 -0800

    created base templates for website

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+from PIL import Image, ImageFilter
+from pprint import PrettyPrinter
+from dotenv import load_dotenv
+import json
+import os
+import random
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """The homepage"""
+    return render_template('home.html')
